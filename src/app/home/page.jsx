8aea93b675cce1f6952b11f4c7348e911549b1dc
darkoto12 <darkoto12@gmail.com>
2025-01-09 13:40:12 +0200

    add features block

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,13 @@
 import Header from '@/components/header'
 import Main from './main'
 import About from './about'
+import Features from './features'
 
 export default function Home() {
   return <>
     <Header />
     <Main />
     <About />
+    <Features />
   </>
 }
